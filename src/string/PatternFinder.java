527ae2ed487755b34c2d57e96edeb67b89e10d3e
Vipul Shukla <shuklavipul7@gmail.com>
2018-03-19 17:01:56 -0400

    new classes added

---
+++ src/string/PatternFinder.java
@@ -0,0 +1,32 @@
+package string;
+
+//This class returns the first match of pattern p in Text t
+//This algorithm uses the brute force method
+public class PatternFinder {
+
+	public static void main(String[] args) {
+		String t = "vipulshukla";
+		String p = "ukl";
+		//should return 3
+		int indexOfFirstMatch = findIndexOfFirstMatch(t, p);
+		System.out.println(indexOfFirstMatch);
+	}
+
+	//Running Time = O(n^2)
+	private static int findIndexOfFirstMatch(String t, String p) {
+		int lenOfT = t.length();
+		int lenOfP = p.length();
+		
+		for (int i = 0; i < lenOfT - lenOfP; i++) {
+			int j = 0;
+			while (j < lenOfP && t.charAt(i + j) == p.charAt(j)) {
+				j++;
+			}
+			if (j == lenOfP) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
